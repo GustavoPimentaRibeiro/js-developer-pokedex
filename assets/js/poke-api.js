7d7c50d8e38977a86b9ec11bc65fd9b8b87a3489
Gustavo Pimenta Ribeiro <63890172+GustavoPimentaRibeiro@users.noreply.github.com>
2024-01-05 23:19:36 -0300

    feat: getting more infos from pokeapi

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -4,6 +4,15 @@ function convertPokeApiDetailToPokemon(pokeDetail) {
   const pokemon = new Pokemon();
   pokemon.number = pokeDetail.id;
   pokemon.name = pokeDetail.name;
+  pokemon.photo = pokeDetail.sprites.other.dream_world.front_default;
+  pokemon.height = pokeDetail.height;
+  pokemon.weight = pokeDetail.weight;
+  pokemon.hp = pokeDetail.stats[0].base_stat;
+  pokemon.attack = pokeDetail.stats[1].base_stat;
+  pokemon.special_attack = pokeDetail.stats[3].base_stat;
+  pokemon.defense = pokeDetail.stats[2].base_stat;
+  pokemon.special_defense = pokeDetail.stats[4].base_stat;
+  pokemon.speed = pokeDetail.stats[5].base_stat;
 
   const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name);
   const [type] = types;
@@ -11,7 +20,7 @@ function convertPokeApiDetailToPokemon(pokeDetail) {
   pokemon.types = types;
   pokemon.type = type;
 
-  pokemon.photo = pokeDetail.sprites.other.dream_world.front_default;
+  console.log(pokemon);
 
   return pokemon;
 }
--- assets/js/pokemon-model.js
+++ assets/js/pokemon-model.js
@@ -4,4 +4,14 @@ class Pokemon {
   type;
   types = [];
   photo;
+
+  height;
+  weight;
+
+  hp;
+  attack;
+  special_attack;
+  defense;
+  special_defense;
+  speed;
 }
